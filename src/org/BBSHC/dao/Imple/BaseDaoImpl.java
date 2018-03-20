5a5069ea3864ec5d7f0e685941db9894b2347868
陶志龙 <1550806350@qq.com>
2018-03-20 09:00:20 +0800

    修改spring配置文件

--- src/org/BBSHC/dao/Imple/BaseDaoImpl.java
+++ src/org/BBSHC/dao/Imple/BaseDaoImpl.java
@@ -16,31 +16,29 @@ import org.hibernate.SessionFactory;
 import org.springframework.orm.hibernate5.HibernateTemplate;
 import org.springframework.orm.hibernate5.support.HibernateDaoSupport;
 
-public class BaseDaoImpl<T> extends HibernateDaoSupport implements BaseDao<T>{
+public class BaseDaoImpl<T> extends HibernateDaoSupport implements BaseDao<T> {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.BB_Second_hand_Car.dao.BaseDao#select(java.lang.Class)
 	 */
 	@Resource
-	public void set(SessionFactory sF){
+	public void set(SessionFactory sF) {
 		super.setSessionFactory(sF);
 	}
+
 	@Resource
 	private SessionFactory sf;
-	//实体类
+	// 实体类
 	@Resource
 	private Class<T> cls;
-	
-	
-	 
+
 	public BaseDaoImpl() {
 		Type type = getClass().getGenericSuperclass();
 		cls = (Class<T>) ((ParameterizedType) type).getActualTypeArguments()[0];
 	}
 
-
-
-	
 	@Override
 	public List<T> select(Class cl, String sql) {
 		Session session = sf.getCurrentSession();
@@ -50,28 +48,19 @@ public class BaseDaoImpl<T> extends HibernateDaoSupport implements BaseDao<T>{
 		return tlist;
 	}
 
-
-
-	
 	@Override
 	public T getOne(Class cl, String id) {
 		Session session = sf.getCurrentSession();
-		T t = (T)session.get(cl, id);
+		T t = (T) session.get(cl, id);
 		return t;
 	}
 
-
-
-	
 	@Override
 	public void delete(T t) {
 		Session session = sf.getCurrentSession();
 		session.delete(t);
 	}
 
-
-
-	
 	@Override
 	public void delete(String id) {
 		Session session = sf.getCurrentSession();
@@ -79,42 +68,34 @@ public class BaseDaoImpl<T> extends HibernateDaoSupport implements BaseDao<T>{
 		session.delete(t);
 	}
 
-
-
-	
 	@Override
 	public void saveOrupdate(T t) {
 		Session session = sf.getCurrentSession();
 		session.saveOrUpdate(t);
 	}
 
-
-
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.BBSHC.dao.BaseDao#selectHQL(java.lang.Class, java.lang.String)
 	 */
 	@Override
 	public List<T> selectHQL(String hql) {
 		HibernateTemplate jdbc = getHibernateTemplate();
-		List<T> tlist = (List<T>)jdbc.find(hql);
+		List<T> tlist = (List<T>) jdbc.find(hql);
 		return tlist;
 	}
 
-
-
-
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.BBSHC.dao.BaseDao#getOneHql(java.lang.Class, java.lang.String)
 	 */
 	@Override
-	public T getOneHql(Class cl,String id) {
+	public T getOneHql(Class cl, String id) {
 		HibernateTemplate jdbc = getHibernateTemplate();
-		T t = (T)jdbc.get(cl, id);
+		T t = (T) jdbc.get(cl, id);
 		return t;
 	}
 
-
-
-	
 }
