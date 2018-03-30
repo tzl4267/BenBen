7f84735e995f4ad7628e9d46f1c8e0e71f5358e6
15971059107 <1009536770@qq.com>
2018-03-30 19:15:17 +0800

    master保存 3.30 xz

--- src/org/BBSHC/dao/BargainRecordDao.java
+++ src/org/BBSHC/dao/BargainRecordDao.java
@@ -3,9 +3,11 @@
  */
 package org.BBSHC.dao;
 
+import java.util.List;
+
 import org.BBSHC.pojo.BargainRecord;
 
 //砍价记录dao接口
 public interface BargainRecordDao extends BaseDao<BargainRecord>{
-
+	List<BargainRecord> selectHQL(String hql);
 }
--- src/org/BBSHC/service/BargainRecordService.java
+++ src/org/BBSHC/service/BargainRecordService.java
@@ -3,8 +3,12 @@
  */
 package org.BBSHC.service;
 
+import java.util.List;
+
 import org.BBSHC.pojo.BargainRecord;
 //砍价记录service接口
 public interface BargainRecordService extends ServiceBase<BargainRecord>{
 	void insertBargainRecord(BargainRecord bd);
+	List<BargainRecord> find();
+	void updateBargainRecord(BargainRecord br);
 }
